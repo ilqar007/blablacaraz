9c9e0e21825ff869fe4d7718177de3a552ac9e4e
Ilgar Mamedov <ilgar.m@bestcomp.net>
2021-02-08 17:57:34 +0400

    duzeli

--- BlaBlaCarAz.Localization/Resources/SharedResource.cs
+++ BlaBlaCarAz.Localization/Resources/SharedResource.cs
@@ -9,5 +9,24 @@ namespace BlaBlaCarAz.Localization.Resources
         public static string Document { get; set; }
         public static string Bag { get; set; }
         public static string Search { get; set; }
+        public static string From { get; set; }
+        public static string To { get; set; }
+        public static string YourRide { get; set; }
+        public static string HomePage { get; set; }
+        public static string Limits { get; set; }
+        public static string Language { get; set; }
+        public static string FindRide { get; set; }
+        public static string OfferRide { get; set; }
+        public static string Register { get; set; }
+        public static string Login { get; set; }
+        public static string Login1 { get; set; }
+        public static string LoginHeader { get; set; }
+        public static string Email { get; set; }
+        public static string Password { get; set; }
+        public static string RememberMe { get; set; }
+        public static string ForgotPassword { get; set; }
+        public static string RegisterAsNewUser { get; set; }
+        public static string LoginHeader2 { get; set; }
+        public static string Privacy { get; set; }
     }
 }
